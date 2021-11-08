85a587620c937a68f85346ddbef13cd4c2a116a6
thangvddz <thangvdph13678@fpt.edu.vn>
2021-11-08 21:15:29 +0700

    GanXongQuanLyPhong8112021

--- src/Utils/Auth.java
+++ src/Utils/Auth.java
@@ -3,26 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Utils;
-
-import Entities.NhanVien;
-
-/**
- *
- * @author you have to better
- */
-public class Auth {
-    public static NhanVien user = null;
-    
-    public static void clear(){
-        Auth.user = null;
-    }
-    
-    public static boolean isLogin(){
-        return Auth.user != null;
-    }
-    
-//    public static boolean isManager(){
-//        return user.isVaiTro();
+//package Utils;
+//
+//import Entities.NhanVien;
+//
+///**
+// *
+// * @author you have to better
+// */
+//public class Auth {
+//    public static NhanVien user = null;
+//    
+//    public static void clear(){
+//        Auth.user = null;
 //    }
-}
+//    
+//    public static boolean isLogin(){
+//        return Auth.user != null;
+//    }
+//    
+////    public static boolean isManager(){
+////        return user.isVaiTro();
+////    }
+//}
